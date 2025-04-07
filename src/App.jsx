f0ad0aebae92ae7d087e1e993605d40892592ecd
Hongju Lee <hongju4385@gmail.com>
2025-04-07 21:42:55 +0900

    [모달 UI] 퐁쥬(이홍주) 제출합니다. (#9)
    * [3단계] : 모달 open/close 기능 구현
    
    * [3단계] : 모달에게 레스토랑 정보 전달

--- src/App.jsx
+++ src/App.jsx
@@ -9,20 +9,38 @@ import { useState } from "react";
 
 function App() {
   const [category, setCategory] = useState("");
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [selectedRestaurant, setSelectedRestaurant] = useState(null);
 
   const filteredRestaurants = restaurants.filter(
     (restaurant) => category === "" || restaurant.category === category
   );
+
+  const handleOpenModal = (restaurant) => {
+    setSelectedRestaurant(restaurant);
+    setIsModalOpen(true);
+  };
+  const handleCloseModal = () => setIsModalOpen(false);
 
   return (
     <>
       <Header />
       <main>
         <CategoryFilter category={category} onChangeCategory={setCategory} />
-        <RestaurantList restaurants={filteredRestaurants} />
+        <RestaurantList
+          restaurants={filteredRestaurants}
+          onRestaurantClick={handleOpenModal}
+        />
       </main>
       <aside>
-        <RestaurantDetailModal />
+        {isModalOpen && (
+          <RestaurantDetailModal
+            isModalOpen={isModalOpen}
+            onCloseEvent={handleCloseModal}
+            selectedRestaurant={selectedRestaurant}
+          />
+        )}
+
         <AddRestaurantModal />
       </aside>
     </>
