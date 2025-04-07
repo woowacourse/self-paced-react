873b5e3e700dc4d9f56e5f6db4b5b324fd3365a9
Hongju Lee <hongju4385@gmail.com>
2025-04-07 16:03:13 +0900

    [1단계] : first-component 완료 (#2)

---
+++ src/Modal/AddRestaurantModal.jsx
@@ -0,0 +1,63 @@
+import "./modal.css";
+
+export default function AddRestaurantModal() {
+  return (
+    <div className="modal modal--open">
+      <div className="modal-backdrop"></div>
+      <div className="modal-container">
+        <h2 className="modal-title text-title">새로운 음식점</h2>
+        <form>
+          <div className="form-item form-item--required">
+            <label htmlFor="category" className="text-caption">
+              카테고리
+            </label>
+            <select name="category" id="category" required autoComplete="off">
+              <option value="">선택해 주세요</option>
+              <option value="한식">한식</option>
+              <option value="중식">중식</option>
+              <option value="일식">일식</option>
+              <option value="양식">양식</option>
+              <option value="아시안">아시안</option>
+              <option value="기타">기타</option>
+            </select>
+          </div>
+
+          <div className="form-item form-item--required">
+            <label htmlFor="name" className="text-caption">
+              이름
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              required
+              autoComplete="organization"
+            />
+          </div>
+
+          <div className="form-item">
+            <label htmlFor="description" className="text-caption">
+              설명
+            </label>
+            <textarea
+              name="description"
+              id="description"
+              cols="30"
+              rows="5"
+              autoComplete="off"
+            ></textarea>
+            <span className="help-text text-caption">
+              메뉴 등 추가 정보를 입력해 주세요.
+            </span>
+          </div>
+
+          <div className="button-container">
+            <button className="button button--primary text-caption">
+              추가하기
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
