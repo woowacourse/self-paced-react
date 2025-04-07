bfa7d173c785459b88bf43986ac89873752f6980
Hongju Lee <hongju4385@gmail.com>
2025-04-07 20:53:57 +0900

    [리스트 렌더링] 퐁쥬(이홍주) 제출합니다. (#8)
    * [1단계] : first-component 완료
    
    * [2단계] : props로 리스트 전달받아서 리스트 출력
    
    * [2단계] : filter 기능 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,22 @@ import RestaurantList from "./RestaurantList/RestaurantList";
 import RestaurantDetailModal from "./Modal/RestaurantDetailModal";
 import AddRestaurantModal from "./Modal/AddRestaurantModal";
 import CategoryFilter from "./Filter/CategoryFilter";
+import { restaurants } from "./Restaurants";
+import { useState } from "react";
 
 function App() {
+  const [category, setCategory] = useState("");
+
+  const filteredRestaurants = restaurants.filter(
+    (restaurant) => category === "" || restaurant.category === category
+  );
+
   return (
     <>
       <Header />
       <main>
-        <CategoryFilter />
-        <RestaurantList />
+        <CategoryFilter category={category} onChangeCategory={setCategory} />
+        <RestaurantList restaurants={filteredRestaurants} />
       </main>
       <aside>
         <RestaurantDetailModal />
