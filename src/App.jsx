32721ae551dfb20801d657aab69ddaee52f131b4
ahhyun <wndkgus0225@naver.com>
2025-04-07 16:11:20 +0900

    jsx로 변환 후 App과 css 파일을 컴포넌트 별로 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
+import "../src/components/Header.jsx";
+import Header from "../src/components/Header.jsx";
+import CategoryFilter from "./components/CategoryFilter.jsx";
+import RestaurantList from "./components/RestaurantList.jsx";
+import RestaurantDetailModal from "./components/RestaurantDetailModal.jsx";
+import AddRestaurantModal from "./components/AddRestaurantModal.jsx";
 
 function App() {
-  return <h1>Self-Paced React</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <CategoryFilter />
+        <RestaurantList />
+      </main>
+      <aside>
+        <RestaurantDetailModal />
+        <AddRestaurantModal />
+      </aside>
+    </>
+  );
 }
 
 export default App;
