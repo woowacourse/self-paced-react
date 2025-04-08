a9ef405990380054a69b2eca8400f80a680f2c90
jin123457 <jin_ox@naver.com>
2025-04-08 14:59:01 +0900

    feat: 레스토랑 목록을 props로 받아 랜더링

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,18 @@ import CategoryFilter from './components/CategoryFilter';
 import Header from './components/Header';
 import RestaurantDetailModal from './components/RestaurantDetailModal';
 import RestaurantList from './components/RestaurantList';
+import { useState } from 'react';
+import { restaurantsData } from './data/restaurantsData';
 
 function App() {
+    const [category, setCategory] = useState('전체');
+    const filteredRestaurants = category === '전체' ? restaurantsData : restaurantsData.filter((restaurant) => restaurant.category === category);
     return (
         <>
             <Header />
             <main>
-                <CategoryFilter />
-                <RestaurantList />
+                <CategoryFilter category={category} onChangeCategory={setCategory} />
+                <RestaurantList restaurants={filteredRestaurants} />
             </main>
             <aside>
                 <RestaurantDetailModal />
---
+++ src/data/convertCategoryData.js
@@ -0,0 +1,8 @@
+export const convertCategoryData = {
+    한식: 'korean',
+    중식: 'chinese',
+    일식: 'japanese',
+    양식: 'western',
+    아시안: 'asian',
+    기타: 'etc',
+};
