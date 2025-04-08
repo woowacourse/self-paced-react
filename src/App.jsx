3fafd25ff0576d84575da9e4706e9bc737b09072
seo_dev <jinsol9909@gmail.com>
2025-04-08 13:39:01 +0900

    feat: jsx 문법으로 변경 후 해당 컴포넌트에 맞는 css 분리 (#5)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
-import "./App.css";
+import './App.css';
+import AddRestaurantModal from './components/AddRestaurantModal';
+import CategoryFilter from './components/CategoryFilter';
+import Header from './components/Header';
+import RestaurantDetailModal from './components/RestaurantDetailModal';
+import RestaurantList from './components/RestaurantList';
 
 function App() {
-  return <h1>Self-Paced React</h1>;
+    return (
+        <>
+            <Header />
+            <main>
+                <CategoryFilter />
+                <RestaurantList />
+            </main>
+            <aside>
+                <RestaurantDetailModal />
+                <AddRestaurantModal />
+            </aside>
+        </>
+    );
 }
 
 export default App;
