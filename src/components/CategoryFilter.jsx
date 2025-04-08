7ce14415b449602ec8371a0e5386566fc8b9ffb2
seo_dev <jinsol9909@gmail.com>
2025-04-08 15:03:06 +0900

    feat: 레스토랑 목록을 props로 받아 랜더링 (#13)

--- src/components/CategoryFilter.jsx
+++ src/components/CategoryFilter.jsx
@@ -1,17 +1,24 @@
 import '../css/CategoryFilter.css';
 
-function CategoryFilter() {
+function CategoryFilter({ category, onChangeCategory }) {
+    const categoryData = ['전체', '한식', '중식', '일식', '양식', '아시안', '기타'];
+
     return (
         <>
             <section className='restaurant-filter-container'>
-                <select name='category' id='category-filter' className='restaurant-filter' aria-label='음식점 카테고리 필터'>
-                    <option value='전체'>전체</option>
-                    <option value='한식'>한식</option>
-                    <option value='중식'>중식</option>
-                    <option value='일식'>일식</option>
-                    <option value='양식'>양식</option>
-                    <option value='아시안'>아시안</option>
-                    <option value='기타'>기타</option>
+                <select
+                    name='category'
+                    id='category-filter'
+                    className='restaurant-filter'
+                    aria-label='음식점 카테고리 필터'
+                    value={category}
+                    onChange={(e) => onChangeCategory(e.target.value)}
+                >
+                    {categoryData.map((category) => (
+                        <option key={category} value={category}>
+                            {category}
+                        </option>
+                    ))}
                 </select>
             </section>
         </>
