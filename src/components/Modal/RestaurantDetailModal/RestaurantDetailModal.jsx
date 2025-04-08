d0566effe37dabf12cbe5d45cbd72780e097a0b2
haku <168343874+ha-kuku@users.noreply.github.com>
2025-04-08 12:36:57 +0900

    [03. 모달 UI 구현하기] 하쿠(이동현) 제출합니다. (#10)
    * style: jsx 문법으로 변경
    
    * refactor: 모듈 분리
    
    * refactor: 식당리스트 동적으로 구현
    
    * refactor; 필터 동적으로 구현
    
    * feat: 상세정보 모달 동작 추가
    
    * feat: 상세정보 모달 동적으로 구현

--- src/components/Modal/RestaurantDetailModal/RestaurantDetailModal.jsx
+++ src/components/Modal/RestaurantDetailModal/RestaurantDetailModal.jsx
@@ -1,17 +1,5 @@
 import './RestaurantDetailModal.css';
 
-function RestaurantDetailModal() {
-  return (
-    <div className="modal modal--open">
-      <div className="modal-backdrop"></div>
-      <div className="modal-container">
-        <h2 className="modal-title text-title">음식점 이름</h2>
-        <div className="restaurant-info">
-          <p className="restaurant-info__description text-body">음식점 소개 문구</p>
-        </div>
-        {/* 닫기 버튼 */}
-        <div className="button-container">
-          <button className="button button--primary text-caption">닫기</button>
         </div>
       </div>
     </div>
--- src/components/RestaurantList/RestaurantList.jsx
+++ src/components/RestaurantList/RestaurantList.jsx
@@ -1,11 +1,9 @@
 import './RestaurantList.css';
 
-function RestaurantList({ restaurants }) {
   return (
     <section className="restaurant-list-container">
       <ul className="restaurant-list">
         {restaurants.map((restaurant) => (
-          <li key={restaurant.id} className="restaurant">
             <div className="restaurant__category">
               <img
                 src={`/category-${getCategoryImageName(restaurant.category)}.png`}
