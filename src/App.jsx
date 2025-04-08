5e9bc07728c29dd36d55e66d0b9eb95f5929b9aa
Beomtae <95863810+Beomtae@users.noreply.github.com>
2025-04-08 13:38:53 +0900

    [Step 1 - 컴포넌트 선언하고 사용하기] 범태(박태범) 미션 제출합니다. (#3)
    * ✨ feat: 템플릿 html를 App.jsx 로 변환
    
    * ✨ feat: App 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,25 @@
 import "./App.css";
 
+import Header from "./components/Header/Header";
+import CategoryFilter from "./components/CategoryFilter/CategoryFilter";
+import RestaurantList from "./components/RestaurantList/RestaurantList";
+import RestaurantDetailModal from "./components/Modal/RestaurantDetailModal";
+import AddRestaurantModal from "./components/Modal/AddRestaurantModal";
+
 function App() {
-  return <h1>Self-Paced React</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <CategoryFilter />
+        <RestaurantList />
+      </main>
+      <aside>
+        <RestaurantDetailModal />
+        <AddRestaurantModal />
+      </aside>
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import "./Header.css";
+
+import addButtonIcon from "../../assets/add-button.png";
+
+function Header() {
+  return (
+    <header className="gnb">
+      <h1 className="gnb__title text-title">점심 뭐 먹지</h1>
+      <button type="button" className="gnb__button" aria-label="음식점 추가">
+        <img src={addButtonIcon} alt="음식점 추가" />
+      </button>
+    </header>
+  );
+}
+
+export default Header;
