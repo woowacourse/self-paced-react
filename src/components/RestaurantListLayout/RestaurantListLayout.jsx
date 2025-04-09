068a6fe6318d763ca90a04d22da446e2cab74d32
dev-dino22 <wlsskdud02@naver.com>
2025-04-09 23:32:05 +0900

    chore: 레스토랑리스트레이아웃 필요없는 제목 제거

--- src/components/RestaurantListLayout/RestaurantListLayout.jsx
+++ src/components/RestaurantListLayout/RestaurantListLayout.jsx
@@ -4,8 +4,7 @@ import RestaurantList from "./RestaurantList/RestaurantList";
 function RestaurantListLayout() {
   return (
     <>
-      <section className="restaurant-list-layout">
-        <h2 className="sr-only">식당 리스트</h2>
+      <section>
         <CategoryFilter />
         <RestaurantList />
       </section>
