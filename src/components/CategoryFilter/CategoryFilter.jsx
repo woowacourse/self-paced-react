c7bc4eaffee1122cdc233b86e40572a136a78188
ahhyun <wndkgus0225@naver.com>
2025-04-09 14:31:13 +0900

    feat: 카테고리 상태에 따라 목록 필터링하여 보여주기

--- src/components/CategoryFilter/CategoryFilter.jsx
+++ src/components/CategoryFilter/CategoryFilter.jsx
@@ -1,21 +1,19 @@
 import "./CategoryFilter.css";
 
-function CategoryFilter() {
+const categories = ["전체", "한식", "중식", "일식", "양식", "아시안", "기타"];
+function CategoryFilter({ category, onChangeCategory }) {
   return (
     <section className="restaurant-filter-container">
       <select
-        name="category"
-        id="category-filter"
         className="restaurant-filter"
-        aria-label="음식점 카테고리 필터"
+        value={category}
+        onChange={(e) => onChangeCategory(e.target.value)}
       >
-        <option value="전체">전체</option>
-        <option value="한식">한식</option>
-        <option value="중식">중식</option>
-        <option value="일식">일식</option>
-        <option value="양식">양식</option>
-        <option value="아시안">아시안</option>
-        <option value="기타">기타</option>
+        {categories.map((c) => (
+          <option key={c} value={c}>
+            {c}
+          </option>
+        ))}
       </select>
     </section>
   );
