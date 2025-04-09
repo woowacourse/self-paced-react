360a564d251c6e3e1e5134e28b0e1d1a377773ea
AHHYUNJU <wndkgus0225@naver.com>
2025-04-09 14:37:14 +0900

    [Step 2 - 목록 UI 구현하기 ] 주렁(주아현) 미션 제출합니다.  (#24)
    * jsx로 변환 후 App과 css 파일을 컴포넌트 별로 분리
    
    * refactor: 불필요한 css 삭제
    
    * refactor: 컴포넌트 기준으로 폴더 구조 변경
    
    * feat: props로 데이터 내려주고 목록 그리기
    
    * feat: 카테고리 상태에 따라 목록 필터링하여 보여주기

---
+++ src/components/CategoryFilter/CategoryFilter.jsx
@@ -0,0 +1,22 @@
+import "./CategoryFilter.css";
+
+const categories = ["전체", "한식", "중식", "일식", "양식", "아시안", "기타"];
+function CategoryFilter({ category, onChangeCategory }) {
+  return (
+    <section className="restaurant-filter-container">
+      <select
+        className="restaurant-filter"
+        value={category}
+        onChange={(e) => onChangeCategory(e.target.value)}
+      >
+        {categories.map((c) => (
+          <option key={c} value={c}>
+            {c}
+          </option>
+        ))}
+      </select>
+    </section>
+  );
+}
+
+export default CategoryFilter;
---
+++ src/components/RestaurantList/RestaurantList.jsx
@@ -0,0 +1,31 @@
+import "./RestaurantList.css";
+
+function RestaurantList({ restaurants }) {
+  return (
+    <section className="restaurant-list-container">
+      <ul className="restaurant-list">
+        {restaurants.map((restaurant) => (
+          <li className="restaurant" key={restaurant.id}>
+            <div className="restaurant__category">
+              <img
+                src={restaurant.image}
+                alt={restaurant.category}
+                className="category-icon"
+              />
+            </div>
+            <div className="restaurant__info">
+              <h3 className="restaurant__name text-subtitle">
+                {restaurant.name}
+              </h3>
+              <p className="restaurant__description text-body">
+                {restaurant.description}
+              </p>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default RestaurantList;
