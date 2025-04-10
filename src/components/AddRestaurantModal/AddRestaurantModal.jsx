54da090d7e323b59854d50233d31d5bf49087ff6
ahhyun <wndkgus0225@naver.com>
2025-04-10 16:44:26 +0900

    feat: 추가하기 버튼 클릭 시 레스토랑 목록에 추가되는 로직 구현

--- src/components/AddRestaurantModal/AddRestaurantModal.jsx
+++ src/components/AddRestaurantModal/AddRestaurantModal.jsx
@@ -1,15 +1,43 @@
+import { useState } from "react";
 import "./AddRestaurantModal.css";
 
-function AddRestaurantModal({ onClose }) {
+function AddRestaurantModal({ onClose, onAdd }) {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [category, setCategory] = useState("");
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+
+    const newRestaurant = {
+      id: Date.now(),
+      name,
+      description,
+      category,
+    };
+
+    onAdd(newRestaurant);
+    onClose();
+  };
+
   return (
     <div className="modal modal--open">
       <div className="modal-backdrop" onClick={onClose}></div>
       <div className="modal-container">
         <h2 className="modal-title text-title">새로운 음식점</h2>
-        <form>
+
+        <form onSubmit={handleAdd}>
           <div className="form-item form-item--required">
-            <label htmlFor="category text-caption">카테고리</label>
-            <select name="category" id="category" required>
+            <label htmlFor="category" className="text-caption">
+              카테고리
+            </label>
+            <select
+              name="category"
+              id="category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              required
+            >
               <option value="">선택해 주세요</option>
               <option value="한식">한식</option>
               <option value="중식">중식</option>
@@ -21,20 +49,41 @@ function AddRestaurantModal({ onClose }) {
           </div>
 
           <div className="form-item form-item--required">
-            <label htmlFor="name text-caption">이름</label>
-            <input type="text" name="name" id="name" required />
+            <label htmlFor="name" className="text-caption">
+              이름
+            </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
           </div>
 
           <div className="form-item">
-            <label htmlFor="description text-caption">설명</label>
-            <textarea name="description" id="description" cols="30" rows="5" />
+            <label htmlFor="description" className="text-caption">
+              설명
+            </label>
+            <textarea
+              name="description"
+              id="description"
+              cols="30"
+              rows="5"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
             <span className="help-text text-caption">
               메뉴 등 추가 정보를 입력해 주세요.
             </span>
           </div>
 
           <div className="button-container">
-            <button className="button button--primary text-caption">
+            <button
+              type="submit"
+              className="button button--primary text-caption"
+            >
               추가하기
             </button>
           </div>
