5467c3cec0c23eb8df3e6add66e6f367d27c5caa
AHHYUNJU <wndkgus0225@naver.com>
2025-04-11 20:29:44 +0900

    [Step 5 - API 연동하기] 주렁(주아현) 미션 제출합니다. (#34)
    * jsx로 변환 후 App과 css 파일을 컴포넌트 별로 분리
    
    * refactor: 불필요한 css 삭제
    
    * refactor: 컴포넌트 기준으로 폴더 구조 변경
    
    * feat: props로 데이터 내려주고 목록 그리기
    
    * feat: 카테고리 상태에 따라 목록 필터링하여 보여주기
    
    * feat: 아이템 클릭 시 정해진 텍스트를 그대로 보여주는 모달 열고 닫기 구현
    
    * feat: 클릭한 아이템의 정보를 모달에 내려줄 수 있도록 변경
    
    * feat: 불필요한 코드 제거
    
    * refactor: restaurants 음식점 리스트 상수를 App 외부에서 선언
    
    * refactor: self-closing 수정
    
    * refactor: 가독성을 위해 변수 이름 변경
    
    * refactor: Restaurant 목록을 구조분해할당을 사용하여  수정
    
    * refactor: 이벤트 핸들러 이름 수정
    
    * refactor: 모달을 닫는 로직 세분화
    
    * feat: 음식점 추가 버튼 활성화
    
    * refactor: 상세 모달에서 restaurant을 구조분해할당하여 수정
    
    * refactor: 불필요한 코드 삭제
    
    * feat: Header에서 레스토랑 추가 폼이 모달로 뜨도록 구현
    
    * feat: 추가하기 버튼 클릭 시 레스토랑 목록에 추가되는 로직 구현
    
    * feat: RestaurantListPage 파일 구현
    
    * feat: API로 레스토랑 목록 연동

--- src/App.jsx
+++ src/App.jsx
@@ -5,49 +5,10 @@ import CategoryFilter from "./components/CategoryFilter/CategoryFilter.jsx";
 import RestaurantList from "./components/RestaurantList/RestaurantList.jsx";
 import RestaurantDetailModal from "./components/RestaurantDetailModal/RestaurantDetailModal.jsx";
 import AddRestaurantModal from "./components/AddRestaurantModal/AddRestaurantModal.jsx";
+import useRestaurants from "./components/hooks/useRestaurant.js";
 
 function App() {
-  const [restaurants, setRestaurants] = useState([
-    {
-      id: "a01",
-      name: "피양콩할마니",
-      description:
-        "평양 출신의 할머니가 수십 년간 운영해온 비지 전문점 피양콩 할마니...",
-      category: "한식",
-    },
-    {
-      id: "a02",
-      name: "친친",
-      description: "정통 중식의 세계를 펼쳐갑니다",
-      category: "중식",
-    },
-    {
-      id: "a03",
-      name: "잇쇼우",
-      description: "정통 자가제면 사누끼 우동이 대표메뉴입니다.",
-      category: "일식",
-    },
-    {
-      id: "a04",
-      name: "이태리키친",
-      description: "늘 변화를 추구하는 이태리키친입니다.",
-      category: "양식",
-    },
-    {
-      id: "a05",
-      name: "호아빈 삼성점",
-      description: "푸짐한 양에 국물이 일품인 쌀국수",
-      category: "아시안",
-    },
-    {
-      id: "a06",
-      name: "도스타코스 선릉점",
-      description: "멕시칸 캐주얼 그릴",
-      category: "기타",
-    },
-
-  ]);
-
+  const { restaurants, fetchRestaurants } = useRestaurants();
   const [category, setCategory] = useState("전체");
   const [addModalOpen, setAddModalOpen] = useState(false);
   const [detailModalOpen, setDetailModalOpen] = useState(false);
@@ -62,7 +23,15 @@ function App() {
   const handleDetailModalClose = () => setDetailModalOpen(false);
 
   const handleAddRestaurant = (newRestaurant) => {
-    setRestaurants((prev) => [...prev, newRestaurant]);
+    fetch("http://localhost:3000/restaurants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newRestaurant),
+    }).then(() => {
+      fetchRestaurants();
+    });
   };
 
   const handleRestaurantItemClick = (restaurant) => {
